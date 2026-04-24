e857e77b1d65d6a24e30da0ed383518155a9d7a0
Alessandro0510 <damianalessandro84@gmail.com>
2026-04-24 00:02:47 -0600

    Muestra información de un proyector usando un modelo de la vista

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -16,13 +16,40 @@ namespace WebApp.Controllers
             //ViewData["Situacion"] = "Bueno";
             //ViewData["FechaDeAlta"] = DateTime.Now.ToString();
 
-            ViewBag.Id = 2;
-            ViewBag.Marca = "Epson";
-            ViewBag.Modelo = "Xlight";
-            ViewBag.NumeroDeSerie = "123456";
-            ViewBag.Situacion = "Bueno";
-            ViewBag.FechaDeAlta = DateTime.Now.ToString();
+            //ViewBag.Id = 2;
+            //ViewBag.Marca = "Epson";
+            //ViewBag.Modelo = "Xlight";
+            //ViewBag.NumeroDeSerie = "123456";
+            //ViewBag.Situacion = "Bueno";
+            //ViewBag.FechaDeAlta = DateTime.Now.ToString();
+            //return View();
+
+            //Uso de modelo de la vista  
+            Proyector proyector = new()
+
+            {
+                Id = 2,
+                Marca = "Epson",
+                Modelo = "Xlight",
+                NumeroDeSerie = "123456",
+                Situacion = SituacionProyector.Bueno,
+                FechaDeAlta = DateTime.Now
+            };
+
+
+            return View(proyector);
+        }
+
+        public IActionResult Privacy()
+
+        {
             return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
